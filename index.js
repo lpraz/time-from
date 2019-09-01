09aba0104d35d5d1fa864c6c27a4bac69cef33dd
Logan Praznik <lpraz@mts.net>
2019-09-01 15:01:29 -0500

    Added Display view

--- index.js
+++ index.js
@@ -3,12 +3,61 @@ import { h, app } from 'hyperapp'
 const getQueryParams = () =>
     new URLSearchParams(window.location.search.substr(1));
 
-const Display = state => (
-    <div>
-        <h1>Some time</h1>
-        <p>until {state.event}</p>
-    </div>
-);
+// TODO: leap years, seconds, etc.?
+const diffTime = (src, dest) => {
+    const diffUnit = (later, earlier, factor) => 
+        Math.floor(later / factor - earlier / factor);
+    
+    const subtractHigherUnit = (lower, higher, factor) => higher === 0 ?
+        lower :
+        lower % factor;
+
+    const per = (before, after) => {
+        const factors = [1000, 60, 60, 24, 365];
+        const units = ["ms", "s", "m", "h", "d", "y"];
+        
+        return factors.slice(units.indexOf(before), units.indexOf(after))
+            .reduce((prev, curr) => prev * curr, 1);
+    };
+
+    let earlier = src > dest ? dest : src;
+    let later = src > dest ? src : dest;
+
+    let count = src > dest ? "up" : "down";
+    let years = diffUnit(later, earlier, per("ms", "y"));
+    let days = subtractHigherUnit(
+        diffUnit(later, earlier, per("ms", "d")),
+        years, per("d", "y"));
+    let hours = subtractHigherUnit(
+        diffUnit(later, earlier, per("ms", "h")),
+        days, per("h", "d"));
+    let minutes = subtractHigherUnit(
+        diffUnit(later, earlier, per("ms", "m")),
+        hours, per("m", "h"));
+    let seconds = subtractHigherUnit(
+        diffUnit(later, earlier, per("ms", "s")),
+        minutes, per("s", "m"));
+    
+    return { count, years, days, hours, minutes, seconds };
+};
+
+const Display = state => {
+    const displayUnit = (amount, unit) => amount > 0 ? (
+        <p><strong>{amount}</strong> {unit}{amount > 1 ? "s" : ""}</p>
+    ) : "";
+
+    let diff = diffTime(state.currentTime, state.targetTime);
+    return (
+        <div>
+            {displayUnit(diff.years, "year")}
+            {displayUnit(diff.days, "day")}
+            {displayUnit(diff.hours, "hour")}
+            {displayUnit(diff.minutes, "minute")}
+            {displayUnit(diff.seconds, "second")}
+            <p>{diff.count == "down" ? "until" : "since"} {state.event}</p>
+        </div>
+    )
+};
 
 const Create = state => (
     <p>Create</p>
@@ -16,7 +65,8 @@ const Create = state => (
 
 app({
     init: () => ({
-        time: getQueryParams().get("time"),
+        currentTime: Date.now(),
+        targetTime: getQueryParams().get("time"),
         event: getQueryParams().get("event")
     }),
     view: state => state.time !== null ? Display(state) : Create(state),
